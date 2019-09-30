ccef33bfed3aa1f1e40085dd583df54e2c9b1980
fred wright <fredwright0@gmail.com>
2019-09-30 20:09:11 +0700

    🎨 improve theme page

--- src/routes/theme.svelte
+++ src/routes/theme.svelte
@@ -21,6 +21,8 @@
       colors: reduce(theme.colors),
       spacing: reduce(theme.spacing),
       boxShadow: reduce(theme.boxShadow),
+      fontSize: reduce(theme.fontSize),
+      fontWeight: reduce(theme.fontWeight),
     }
   }
 </script>
@@ -31,84 +33,90 @@
   export let colors
   export let spacing
   export let boxShadow
+  export let fontSize
+  export let fontWeight
 </script>
 
 <style>
-  .wrapper {
+  .triple {
+    column-count: 3;
+  }
+  .double {
+    column-count: 2;
+  }
+  .item {
     display: grid;
     grid-gap: 1rem;
-    grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+    grid-template-columns: 1fr min-content 1fr;
+  }
+  .item + .item {
+    @apply mt-2;
   }
   .swatch {
     @apply rounded;
     @apply border;
-    @apply ml-4;
     width: 32px;
     height: 32px;
   }
 </style>
 
-<div class="wrapper">
-  <div>
-    <section class="bg-white rounded px-4 pb-4 pt-4">
-      <Heading>colors</Heading>
-      <table>
-        <tbody>
-          {#each Object.entries(colors) as [key, value]}
-            <tr>
-              <th class="text-indigo-600 text-xs uppercase text-right pr-4">
-                {key}
-              </th>
-              <td>{value}</td>
-              <td>
-                <div class="swatch" style={`background-color:${value};`} />
-              </td>
-            </tr>
-          {/each}
-        </tbody>
-      </table>
-    </section>
-  </div>
-
-  <div>
-    <section class="bg-white rounded px-4 pb-4 pt-4">
-      <Heading>spacing</Heading>
-      <table>
-        <tbody>
-          {#each Object.entries(spacing) as [key, value]}
-            <tr>
-              <th class="text-indigo-600 text-xs uppercase text-right pr-4">
-                {key}
-              </th>
-              <td>{value}</td>
-              <td>
-                <div class="swatch" style={`width:${value};height:${value};`} />
-              </td>
-            </tr>
-          {/each}
-        </tbody>
-      </table>
-    </section>
-  </div>
-
-  <div>
-    <section class="bg-white rounded px-4 pb-4 pt-4">
-      <Heading>boxShadow</Heading>
-      <table>
-        <tbody>
-          {#each Object.entries(boxShadow) as [key, value]}
-            <tr>
-              <th class="text-indigo-600 text-xs uppercase text-right pr-4">
-                {key}
-              </th>
-              <td>{value}</td>
-              <td>
-                <div class="swatch" style={`box-shadow:${value};`} />
-              </td>
-            </tr>
-          {/each}
-        </tbody>
-      </table>
-    </section>
-  </div>
-</div>
+<section class="bg-white rounded px-4 pb-4 pt-4 mb-8">
+  <Heading>colors</Heading>
+  <ul class="triple">
+    {#each Object.entries(colors) as [key, value]}
+      <li class="item">
+        <p class="text-indigo-600 text-xs uppercase text-right">{key}</p>
+        <div class="swatch" style={`background-color:${value};`} />
+        <p>{value}</p>
+      </li>
+    {/each}
+  </ul>
+</section>
+<section class="bg-white rounded px-4 pb-4 pt-4 mb-8">
+  <Heading>spacing</Heading>
+  <ul class="triple">
+    {#each Object.entries(spacing) as [key, value]}
+      <li class="item">
+        <p class="text-indigo-600 text-xs uppercase text-right">{key}</p>
+        <div class="swatch" style={`height:${value};`} />
+        <p>{value}</p>
+      </li>
+    {/each}
+  </ul>
+</section>
+<section class="bg-white rounded px-4 pb-4 pt-4 mb-8">
+  <Heading>boxShadow</Heading>
+  <ul class="triple">
+    {#each Object.entries(boxShadow) as [key, value]}
+      <li class="item">
+        <p class="text-indigo-600 text-xs uppercase text-right">{key}</p>
+        <div class="swatch" style={`box-shadow:${value};`} />
+        <p>{value}</p>
+      </li>
+    {/each}
+  </ul>
+</section>
+<section class="bg-white rounded px-4 pb-4 pt-4 mb-8">
+  <Heading>fontSize</Heading>
+  <ul class="double">
+    {#each Object.entries(fontSize) as [key, value]}
+      <li class="item">
+        <p class="text-indigo-600 text-xs uppercase text-right">{key}</p>
+        <p style={`font-size:${value};`}>Somnambulist</p>
+        <p>{value}</p>
+      </li>
+    {/each}
+  </ul>
+</section>
+<section class="bg-white rounded px-4 pb-4 pt-4 mb-8">
+  <Heading>fontWeight</Heading>
+  <ul class="double">
+    {#each Object.entries(fontWeight) as [key, value]}
+      <li class="item">
+        <p class="text-indigo-600 text-xs uppercase text-right">{key}</p>
+        <p style={`font-weight:${value};`}>Somnambulist</p>
+        <p>{value}</p>
+      </li>
+    {/each}
+  </ul>
+</section>
